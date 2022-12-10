b71b4ed4ea0b61d224367ac17e22734f3648cd2a
zainarhn <zainaraihan@gmail.com>
2022-12-10 08:18:04 +0700

    first commit

---
+++ src/components/Card/index.js
@@ -0,0 +1,37 @@
+import "./style.css";
+import { useState, useEffect } from "react";
+
+const Card = () => {
+  const [card, setCard] = useState("");
+
+  const surah = [];
+
+  useEffect(() => {
+    fetch("https://equran.id/api/surat")
+      .then((response) => response.json())
+      .then((data) => {
+        data.forEach((sur, id) => {
+          surah.push(
+            <div className="cardList" key={id}>
+              <div className="glass">
+                <div className="namaSurah">
+                  {sur.nomor}.{sur.nama_latin}
+                </div>
+                <div className="preview">
+                  <div className="potonganSurah">{sur.nama}</div>
+                  <div className="artiTurun">
+                    {sur.arti} - {sur.tempat_turun}
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        });
+        setCard(surah);
+      });
+  });
+
+  return <div className="card">{card}</div>;
+};
+
+export default Card;
