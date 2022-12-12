39f48e47b37ae09dd0903ec84a0487fc74ce5e5c
zainarhn <zainaraihan@gmail.com>
2022-12-12 21:29:49 +0700

    merubah warna card menjadi lebih gelap

--- src/components/Surah/index.js
+++ src/components/Surah/index.js
@@ -1,7 +1,21 @@
+import { useParams } from "react-router";
+import { useEffect } from "react";
+
 const Surah = () => {
+  const params = useParams();
+  useEffect(() => {
+    fetch("https://equran.id/api/surat/" + params.id)
+      .then((res) => res.json())
+      .then((data) => console.log(data.ayat));
+  });
+
+  const color = {
+    color: "white",
+  };
+
   return (
     <div className="surah">
-      <p>isi surah</p>
+      <p style={color}>isi surah</p>
     </div>
   );
 };
