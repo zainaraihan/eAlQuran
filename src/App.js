044565f3857c4d8e1bca6501c90abb14a86b78f0
zainarhn <zainaraihan@gmail.com>
2022-12-25 14:32:14 +0700

    menambahkan navbar pada web

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Card from "./components/Card/index";
 import Surah from "./components/Surah/index";
+import Nav from "./components/Nav/index";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter className="app">
+      <Nav />
       <Routes>
         <Route path="/">
           <Route index element={<Card />} />
---
+++ src/components/Nav/index.js
@@ -0,0 +1,16 @@
+import "./style.css";
+
+const Nav = () => {
+  return (
+    <div className="Nav">
+      <div className="logo">Logo</div>
+      <div className="kanan">
+        <div className="search">Search</div>
+        <div className="about">About</div>
+        <div className="menu">Menu</div>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
