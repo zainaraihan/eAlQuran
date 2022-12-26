a42823b3c8a4e5b1af09589e8d7779cf513e942a
zainarhn <zainaraihan@gmail.com>
2022-12-26 22:57:53 +0700

    merubah sedikit pada navbar

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -3,11 +3,12 @@ import "./style.css";
 const Nav = () => {
   return (
     <div className="Nav">
-      <div className="logo">Logo</div>
+      <div className="logo">
+        <img src="https://ealquran.netlify.app/favicon.ico" alt="" />
+      </div>
       <div className="kanan">
         <div className="search">Search</div>
         <div className="about">About</div>
-        <div className="menu">Menu</div>
       </div>
     </div>
   );
